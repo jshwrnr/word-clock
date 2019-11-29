30da55e9d8be65b5bc8d98c18af91ebcf9b06d42
jshwrnr <joshwerner001@gmail.com>
2019-11-29 09:53:36 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./css/master.css";
+import WordClock from "./components/WordClock";
+
+const App = () => (
+  <div className="position-absolute">
+    <WordClock />
+  </div>
+);
+
+export default App;
---
+++ src/components/WordClock/WordClock.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class WordClock extends Component {
+  render() {
+    return <div>clock</div>;
+  }
+}
+
+export default WordClock;
